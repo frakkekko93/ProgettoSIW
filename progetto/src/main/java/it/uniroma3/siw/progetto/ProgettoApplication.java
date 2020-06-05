04cc15f3cb83a08878dc827206430023575ffbef
Ditommy98 <Ditommy98@users.noreply.github.com>
2020-06-05 14:39:25 +0200

    oauth2
    oauth2

--- progetto/src/main/java/it/uniroma3/siw/progetto/ProgettoApplication.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/ProgettoApplication.java
@@ -1,10 +1,22 @@
 package it.uniroma3.siw.progetto;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+@RestController
 public class ProgettoApplication {
+	
+	@GetMapping("/")
+	public String message(Principal principal) {
+		return "Hi "+principal.getName()+" welcome to MammitaApplication";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoApplication.class, args);
