a322a3f506dc7e90c8d7ba046308f13e37315557
Ditommy98 <Ditommy98@users.noreply.github.com>
2020-06-05 17:56:58 +0200

    oauthFinal

--- progetto/src/main/java/it/uniroma3/siw/progetto/ProgettoApplication.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/ProgettoApplication.java
@@ -1,21 +1,41 @@
 package it.uniroma3.siw.progetto;
 
-import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @RestController
-public class ProgettoApplication {
-	
-	@GetMapping("/")
-	public String message(Principal principal) {
-		return "Hi "+principal.getName()+" welcome to MammitaApplication";
+public class ProgettoApplication extends WebSecurityConfigurerAdapter {
+
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.authorizeRequests(a -> a
+				.antMatchers("/", "/error", "/webjars/**").permitAll()
+				.anyRequest().authenticated()
+			)
+			.exceptionHandling(e -> e
+				.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			)
+			.oauth2Login();
+		// @formatter:on
 	}
 
 	public static void main(String[] args) {
