a4d8255cb72f91e354b2feaa8df17d2cf23149ad
Ditommy98 <Ditommy98@users.noreply.github.com>
2020-06-06 15:57:56 +0200

    user

--- progetto/src/main/java/it/uniroma3/siw/progetto/controller/MainController.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/controller/MainController.java
@@ -1,5 +1,10 @@
 package it.uniroma3.siw.progetto.controller;
 
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
+	
 
 	@GetMapping("index")
     public String index()
