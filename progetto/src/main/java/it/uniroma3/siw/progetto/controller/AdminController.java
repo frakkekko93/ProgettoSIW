a055c84a089b44b323c3bfa57def61aae3bb5a52
Ditommy98 <Ditommy98@users.noreply.github.com>
2020-06-16 11:07:48 +0200

    deleteUser FINAL

--- progetto/src/main/java/it/uniroma3/siw/progetto/controller/AdminController.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/controller/AdminController.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.progetto.controller;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import it.uniroma3.siw.progetto.model.Progetto;
 import it.uniroma3.siw.progetto.model.Ruolo;
 import it.uniroma3.siw.progetto.model.Utente;
+import it.uniroma3.siw.progetto.service.ProgettoService;
 import it.uniroma3.siw.progetto.service.RuoloService;
 import it.uniroma3.siw.progetto.service.UtenteService;
 
@@ -23,6 +26,9 @@ public class AdminController {
 	
 	@Autowired
 	protected RuoloService ruoloService;
+	
+	@Autowired
+	protected ProgettoService progettoService;
 
 	/* Visualizza la lista di tutti gli utenti  */
 	@RequestMapping(value = { "/list" }, method = RequestMethod.GET)
@@ -46,14 +52,13 @@ public class AdminController {
 		model.addAttribute("utente", utente);
 		model.addAttribute("ruolo", ruolo);
 		
-		
 
 		return "userToDelete";
 	}
 	
 	/* Cancella un progetto */
 	@RequestMapping(value= {"/deleteUtente"}, method = RequestMethod.POST)
-	public String deleteUtente(HttpServletRequest request)
+	public String deleteUtente(HttpServletRequest request, Model model)
 	{
 		Utente utente = this.utenteService.getUtente(Long.parseLong(request.getParameter("utente")));
 
@@ -61,8 +66,24 @@ public class AdminController {
 
 		if(comando.equals("elimina")) 
 		{
+			List<Progetto> visibili = utente.getProgettiVisibili();
+		    Iterator<Progetto> iterator = visibili.iterator();
+		    while(iterator.hasNext()) 
+		    {
+		    	Progetto p = iterator.next();
+		    	p.getMembri().clear();
+		    	this.progettoService.save(p);
+		    }
+		    
+		    Ruolo r = this.ruoloService.getRuolo(utente);
+		    this.ruoloService.delete(r);
+		    
 			utenteService.delete(utente);
 		}
+		
+		List<Utente> utenti = this.utenteService.getAllUsers();
+		model.addAttribute("utenti", utenti);
+		
 
 		return "userList";
 	}
