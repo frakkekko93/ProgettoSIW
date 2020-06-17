19e0d5bae796e6b64f75c5a4015d64a293aed015
frakkekko93 <62305534+frakkekko93@users.noreply.github.com>
2020-06-17 11:56:39 +0200

    Bug fixing and style setting

--- progetto/src/main/java/it/uniroma3/siw/progetto/service/ProgettoService.java
+++ progetto/src/main/java/it/uniroma3/siw/progetto/service/ProgettoService.java
@@ -1,11 +1,14 @@
 package it.uniroma3.siw.progetto.service;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import it.uniroma3.siw.progetto.model.Progetto;
+import it.uniroma3.siw.progetto.model.Tag;
+import it.uniroma3.siw.progetto.model.Task;
 import it.uniroma3.siw.progetto.model.Utente;
 import it.uniroma3.siw.progetto.repository.ProgettoRepository;
 
@@ -49,6 +52,46 @@ public class ProgettoService
         progetto.aggiungiMembro(utente);
         return this.progettoRepository.save(progetto);
     }
+	
+	/* Verifica se il progetto ha un task con il nome specificato */
+	public boolean hasTask(String nome, Progetto progetto)
+	{
+		List<Task> tasks = progetto.getTasks();
+		Iterator<Task> it = tasks.iterator();
+		
+		Task t = null;
+		while(it.hasNext())
+		{
+			t = it.next();
+			
+			if(t.getNome().equals(nome))
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/* Verifica se il progetto ha un tag con il nome specificato */
+	public boolean hasTag(String nome, Progetto progetto)
+	{
+		List<Tag> tags = progetto.getTags();
+		Iterator<Tag> it = tags.iterator();
+		
+		Tag t = null;
+		while(it.hasNext())
+		{
+			t = it.next();
+			
+			if(t.getNome().equals(nome))
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
 	
 	/* Elimina progetto */
 	@Transactional
